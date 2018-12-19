bd490897f0939d8b66c6fbb3221ec2421eda7f24
Raphael Thomazella <thomazella9@gmail.com>
2018-12-19 09:03:53 -0200

    store file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
-import reducer from './reducers'
-
-const store = createStore(reducer)
+import store from "./store";
 
 render(
   <Provider store={store}>
