078e662debf0ba698b547391764ef42985fdc7d4
Raphael Thomazella <thomazella9@gmail.com>
2018-12-19 09:06:12 -0200

    logger

--- src/store.js
+++ src/store.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux'
-import reducer from './reducers'
+import { createStore, applyMiddleware } from "redux";
+import reducer from "./reducers";
+import logger from "redux-logger";
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  applyMiddleware(logger)
+)
 
 export default store;
+
