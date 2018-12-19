c11b0a0296923dfae6da2cbc1ae88cc807250972
Raphael Thomazella <thomazella9@gmail.com>
2018-12-19 10:56:08 -0200

    action file loop run into babel problem

--- src/lidux.js
+++ src/lidux.js
@@ -1,7 +1,46 @@
-const { resolve } = require("path");
-const config = require(resolve("liduxrc"));
+const { resolve: re } = require("path");
+const { readdirSync, statSync } = require("fs");
 
-console.log("config", config);
+const config = require(re("liduxrc"));
+const { actionsRoot, transformState } = config;
+
+const nodes = readdirSync(re(actionsRoot));
+const paths = nodes.map(node => re(`${actionsRoot}/${node}`));
+
+const topLevel = paths.map((path, i) => {
+  const stats = statSync(path);
+  return {
+    stats,
+    path,
+    isDir: stats.isDirectory(),
+    isFile: !stats.isDirectory(),
+    file: nodes[i]
+  };
+});
+
+const dirs = topLevel.filter(it => it.isDir);
+const files = topLevel.filter(it => it.isFile);
+
+const actionFiles = [];
+for (const dir of dirs) {
+  const files = readdirSync(dir.path);
+  const result = files.map(file => {
+    return {
+      file,
+      path: `${dir.path}/${file}`,
+      relativePath: `${actionsRoot}/${file}`
+    };
+  });
+  actionFiles.push(...result);
+};
+
+console.log("action files", actionFiles);
+
+for (const actionFile of actionFiles) {
+  const { reducer } = require(actionFile.path)
+  console.log("red", reducer)
+}
+
 const reducer = (state, action) => {
   return state;
 };
