777e3fe51fe59d5b01c15211a4b13ea71724d310
Raphael Thomazella <thomazella9@gmail.com>
2019-02-23 00:22:52 -0300

    wip emit declaration, fix tests

--- jest.config.js
+++ jest.config.js
@@ -66,14 +66,14 @@ module.exports = {
   // ],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "json",
-  //   "jsx",
-  //   "ts",
-  //   "tsx",
-  //   "node"
-  // ],
+  moduleFileExtensions: [
+    'js',
+    // "json",
+    // "jsx",
+    'ts',
+    // "tsx",
+    // "node"
+  ],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
